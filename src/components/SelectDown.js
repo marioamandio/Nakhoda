978a9bf82ee8f3e520e5de2fe7afe926fcb6069e
marioamandio <30850605+marioamandio@users.noreply.github.com>
2019-01-14 17:44:05 +0000

    finishing select btn

--- src/components/SelectDown.js
+++ src/components/SelectDown.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import '../styles/elements.css'
+import '../styles/elements.css';
+
 
 
 
@@ -13,18 +14,29 @@ class SelectDown extends Component {
     }
 
     renderOptions() {
-        const fills = new Array(7).fill(`some random text`);
+        const fills = new Array(7).fill(`some random text sim`);
         return (
             <div className="select-menu-list">
-                {fills.map((fill, i) => <div key={i}>{fill}</div>)}
+                {fills.map((fill, i) => (
+                    <div 
+                        key={i} 
+                        className={`select-menu__options--option`} 
+                        id={`menu${i}`}>
+                            <div className="select-menu__options--option--text">
+                                {fill}
+                            </div>
+                    </div>))}
             </div>
         )
     }
 
     render() {
         return (
-            <div>
+            <div className="select-menu">
                 <div className="select-menu__custom" name="some" id="" onClick={this.onClickHandler.bind(this)}>
+                    <p>something here</p>
+                </div>
+                <div className="select-menu__options">
                     {this.state.selected && this.renderOptions()}
                 </div>
             </div>
